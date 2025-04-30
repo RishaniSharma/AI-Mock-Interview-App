02913187cdbecdf0df8bf526913d53271277d3b1
RishaniSharma <144053500+RishaniSharma@users.noreply.github.com>
2025-04-30 17:34:33 +0000

    commit9

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -11,12 +11,61 @@ import {
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
+import { chatSession } from '@/utils/GeminiAIModal'
+import { LoaderCircle } from 'lucide-react'
+import { db } from '@/utils/db'
+import { MockInterview } from '@/utils/schema'
+import {v4 as uuidv4} from 'uuid';
+import { useUser } from '@clerk/nextjs'
+import moment from 'moment'
+
 
 function AddNewInterview() {
     const [openDialog,setOpenDialog]=useState(false)
-    const [JobPosition,setJobPosition]=useState();
-    const [JobDesc,setJobDesc]=useState();
-    const [JobExperience,setJobExperience]=useState();
+    const [jobPosition,setJobPosition]=useState();
+    const [jobDesc,setJobDesc]=useState();
+    const [jobExperience,setJobExperience]=useState();
+    const [loading,setLoading]=useState(false);
+    const [jsonReponse,setJsonResponse]=useState([]);
+    const {user}=useUser();
+
+    const onSubmit=async(e)=>{
+      setLoading(true);
+      e.preventDefault()
+      console.log(jobPosition, jobDesc, jobExperience);
+
+      const InputPrompt="Job Position: "+jobPosition+", Job Description: "+jobDesc+", Years of Experience: "+jobExperience+". Depending on this information, please give me "+process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT+" interview questions with answers in JSON format. Give question and answers as field in JSON.";
+
+
+      const result= await chatSession.sendMessage(InputPrompt);
+
+      const MockJsonResp=(result.response.text()).replace('```json','').replace('```','');
+
+      console.log(JSON.parse(MockJsonResp));
+      setJsonResponse(MockJsonResp);
+
+      if(MockJsonResp){
+      const resp= await db.insert(MockInterview)
+      .values({
+        mockId:uuidv4(),
+        jsonMockResp:MockJsonResp,
+        jobPosition:jobPosition,
+        jobDesc:jobDesc,
+        jobExperience:jobExperience,
+        createdBy:user?.primaryEmailAddress?.emailAddress,
+        createdAt:moment().format('DD-MM-yyyy')
+      }).returning({mockId:MockInterview.mockId});
+
+      console.log("Inserted ID:",resp);
+      if(resp){
+        setOpenDialog(false);
+      }
+    }
+    else{
+      console.log("ERROR");
+    }
+      setLoading(false);
+    }
   return (
     <div>
         <div className='p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all'
@@ -29,30 +78,40 @@ function AddNewInterview() {
     <DialogHeader>
       <DialogTitle className='text-2xl'>Tell us more about your job interview</DialogTitle>
       <DialogDescription>
-        <form>
+        <form onSubmit={onSubmit}>
         <div>
             <h2>Add details about job position, your skills and years of experience</h2>
 
             <div className='mt-7 my-3'>
               <label>Job Position/ Role Name</label>
               <Input placeholder="Ex. Full Stack Developer" required 
-              onChange
+              onChange={(event)=>setJobPosition(event.target.value)}
               />
             </div>
 
             <div className='my-3'>
               <label>Job Description/ Tech Stack in short</label>
-              <Textarea placeholder="Ex. React, Angular, NodeJS, MySQL" required />
+              <Textarea placeholder="Ex. React, Angular, NodeJS, MySQL" required
+              onChange={(event)=>setJobDesc(event.target.value)}
+              />
             </div>
 
             <div className='my-3'>
               <label>Experience in years</label>
-              <Input placeholder="Ex. 5" max="50" type="number" required />
+              <Input placeholder="Ex. 5" max="50" type="number" required
+              onChange={(event)=>setJobExperience(event.target.value)}
+              />
             </div>
         </div>
         <div className='flex gap-5 justify-end'>
             <Button type="button" variant="ghost" onClick={()=>setOpenDialog(false)}>Cancel</Button>
-            <Button type="submit">Start Interview</Button>
+            <Button type="submit" disabled={loading}>
+              {loading?
+              <>
+              <LoaderCircle className='animate-spin' />'Generating from AI'
+              </>:'Start Interview'
+            }
+            </Button>
         </div>
         </form>
       </DialogDescription>
