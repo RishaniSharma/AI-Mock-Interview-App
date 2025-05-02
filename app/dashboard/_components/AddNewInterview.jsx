30ede22102ad01be5d3dd43f22a699bf01fe4941
RishaniSharma <144053500+RishaniSharma@users.noreply.github.com>
2025-05-02 17:56:41 +0000

    comit10

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -18,6 +18,7 @@ import { MockInterview } from '@/utils/schema'
 import {v4 as uuidv4} from 'uuid';
 import { useUser } from '@clerk/nextjs'
 import moment from 'moment'
+import { useRouter } from 'next/navigation'
 
 
 function AddNewInterview() {
@@ -28,6 +29,7 @@ function AddNewInterview() {
     const [loading,setLoading]=useState(false);
     const [jsonReponse,setJsonResponse]=useState([]);
     const {user}=useUser();
+    const router=useRouter();
 
     const onSubmit=async(e)=>{
       setLoading(true);
@@ -59,6 +61,7 @@ function AddNewInterview() {
       console.log("Inserted ID:",resp);
       if(resp){
         setOpenDialog(false);
+        router.push('/dashboard/interview/'+ resp[0]?.mockId)
       }
     }
     else{
