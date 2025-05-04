746dfd8a3079a8899e277f9077f1b0ee304479dd
RishaniSharma <144053500+RishaniSharma@users.noreply.github.com>
2025-05-04 16:11:36 +0000

    commit11

---
+++ app/dashboard/_components/interview/[interviewId]/start/page.jsx
@@ -0,0 +1,42 @@
+"use client"
+import { db } from '@/utils/db';
+import { MockInterview } from '@/utils/schema';
+import { eq } from 'drizzle-orm';
+import React, { useEffect, useState } from 'react'
+
+function StartInterview({params}) {
+
+    const [interviewData, setInterviewData]=useState();
+    const [mockInterviewQuestion, setMockInterviewQuestion]=useState();
+
+    useEffect(()=>{
+      GetInterviewDetails();
+    },[])
+
+    /**
+       * used to get interview details from mockId/Interview Id 
+       */
+      const GetInterviewDetails = async () => {
+        const result = await db
+          .select()
+          .from(MockInterview)
+          .where(eq(MockInterview.mockId, params.interviewId));
+        
+          const jsonMockResp= JSON.parse(result[0].jsonMockResp);
+          console.log(jsonMockResp);
+          setMockInterviewQuestion(jsonMockResp);
+          setInterviewData(result[0]);
+      };
+
+  return (
+    <div>
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            {/* Questions  */}
+
+            {/* video/audio recording  */}
+        </div>
+    </div>
+  )
+}
+
+export default StartInterview
